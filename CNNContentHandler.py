3177652cb3a46d54d305a700dd7d4246f93fdac2
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2022-12-15 16:02:43 +0100

    Fix scripts to work with current website
     - added option to save file on keyboard interupt
     - removed alternate_title and part_of fields as they don't seem to exist anymore
     - fixed bs4 queries to work on latest version of CNN

--- CNNContentHandler.py
+++ CNNContentHandler.py
@@ -11,45 +11,53 @@ class CNNContentHandler:
         self.dataset = pd.DataFrame()
 
     def create_empty_article(self):
-        self.CNN_article = {'author': '',
-                            'date_published': '',
-                            'part_of': '',
-                            'article_section': '',
-                            'url': '',
-                            'headline': '',
-                            'description': '',
-                            'keywords': '',
-                            'alternative_headline': '',
-                            'text': ''}
+        self.CNN_article = {
+            'author': '',
+            'date_published': '',
+            'date_modified': '',
+            'article_section': '',
+            'url': '',
+            'headline': '',
+            'description': '',
+            'keywords': '',
+            'text': ''
+        }
 
     def file_article(self, html):
         is_article = True
         soup = BeautifulSoup(html, 'html.parser')
         for script in soup(["script", "style"]):
             script.extract()
-        try:
-            self.CNN_article['author'] = soup.find(itemprop="author").attrs['content']
-            self.CNN_article['date_published'] = soup.find(itemprop="datePublished").attrs['content']
-            self.CNN_article['part_of'] = soup.find(itemprop="isPartOf").attrs['content']
-            self.CNN_article['article_section'] = soup.find(itemprop="articleSection").attrs['content']
-            self.CNN_article['url'] = soup.find(itemprop="url").attrs['content']
-            self.CNN_article['headline'] = soup.find(itemprop="headline").attrs['content']
-            self.CNN_article['description'] = soup.find(itemprop="description").attrs['content']
-            self.CNN_article['keywords'] = soup.find(itemprop="keywords").attrs['content']
-            self.CNN_article['alternative_headline'] = soup.find(itemprop="alternativeHeadline").attrs['content']
-            self.CNN_article['text'] = soup.find(id="body-text").text
 
+        page_type = soup.find("meta", {"name": "type"})
+        if not page_type or page_type.attrs['content'] != "article":
+            self.logger.info(f'Page is not of type "article". Actual type is: {page_type[:10] if page_type is str else None}')
+            return False
+
+        try:
+            self.CNN_article['author'] = soup.find("meta", {"name": "author"}).attrs['content'].strip()
+            self.CNN_article['date_published'] = soup.find("meta", {"property": "article:published_time"}).attrs['content'].strip()
+            self.CNN_article['date_modified'] = soup.find("meta", {"property": "article:modified_time"}).attrs['content'].strip()
+            self.CNN_article['article_section'] = soup.find("meta", {"name": "meta-section"}).attrs['content']
+            self.CNN_article['url'] = soup.find("meta", {"property": "og:url"}).attrs['content']
+            self.CNN_article['headline'] = soup.find("h1", {"data-editable": "headlineText"}).text.strip()
+            self.CNN_article['description'] = soup.find("meta", {"name": "description"}).attrs['content'].strip()
+            self.CNN_article['keywords'] = soup.find("meta", {"name": "keywords"}).attrs['content'].strip()
+            self.CNN_article['text'] = soup.find(itemprop="articleBody").text.strip()
         except Exception as err:
             is_article = False
+            self.logger.info(f'Missing Article dataError: {err}')
+            for k, v in self.CNN_article.items():
+                self.logger.info(f"{k}: {None if not v else v[:10] + '...'}")
             self.logger.info(f'Missing Article dataError: {err}')
 
         return is_article
 
     def add_article(self):
-        self.dataset = self.dataset.append(self.CNN_article, ignore_index=True)
+        self.dataset = self.dataset.concat(self.CNN_article, ignore_index=True)
 
-    def save_content(self):
-        self.dataset.to_csv('CNN_Articles.csv')
+    def save_content(self, filename):
+        self.dataset.to_csv(filename)
 
     def process_page(self, html):
         self.create_empty_article()
--- WebCrawler.py
+++ WebCrawler.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from collections import OrderedDict
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
@@ -7,12 +8,13 @@ from bs4 import BeautifulSoup
 
 class WebCrawler:
 
-    def __init__(self, content_handler, urls=None, stop_depth=sys.maxsize):
+    def __init__(self, content_handler, save_file="CNN_Articles.csv", urls=None, stop_depth=sys.maxsize):
         self.logger = logging.getLogger(__name__)
         self.content_handler = content_handler
+        self.save_file = save_file
         self.stop_depth = stop_depth
-        self.visited_urls = []
-        self.urls_to_visit = [(url, 0) for url in urls]
+        self.visited_urls = set()
+        self.urls_to_visit = OrderedDict([(x, 0) for x in urls])
 
     def download_url_text(self, url):
         headers = {
@@ -28,14 +30,18 @@ class WebCrawler:
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             path = link.get('href')
-            if path and path.startswith('/'):
+            if not path:
+                continue
+            if path.startswith('/'):
                 path = urljoin(url, path)
+            if not path.startswith('https://edition.cnn.com/'):
+                continue
             yield path
 
     def add_url_to_visit(self, url):
         if url[1] <= self.stop_depth:
-            if url[0] not in self.visited_urls and url not in self.urls_to_visit:
-                self.urls_to_visit.append(url)
+            if url[0] not in self.visited_urls:
+                self.urls_to_visit[url[0]] = url[1]
 
     def crawl(self, url):
         html = self.download_url_text(url[0])
@@ -44,14 +50,19 @@ class WebCrawler:
             self.add_url_to_visit((curr_url, url[1] + 1))
 
     def run(self):
-        while self.urls_to_visit:
-            curr_url = self.urls_to_visit.pop(0)
-            self.logger.info(f'Crawling: {curr_url[0]}, Depth: {curr_url[1]}')
-            try:
-                self.crawl(curr_url)
-            except Exception as err:
-                self.logger.exception(f'Exception crawl: {curr_url[0]},  Depth: {curr_url[1]}, Error: {err}')
-            finally:
-                self.visited_urls.append(curr_url[0])
-
-        self.content_handler.save_content()
+        try:
+            while self.urls_to_visit:
+                curr_url = self.urls_to_visit.popitem(last=False)
+                self.logger.info(f'Crawling: {curr_url[0]}, Depth: {curr_url[1]}, Remaining urls: {len(self.urls_to_visit)}')
+                try:
+                    self.crawl(curr_url)
+                except Exception as err:
+                    self.logger.exception(f'Exception crawl: {curr_url[0]},  Depth: {curr_url[1]}, Error: {err}')
+                finally:
+                    self.visited_urls.add(curr_url[0])
+        except KeyboardInterrupt:
+            user_input = input(f"\n\nKeyboard interrupt, do you want to save content in {self.save_file}? (Y/n): ")
+            if user_input.lower() in ["no", "n"]:
+                return
+            pass
+        self.content_handler.save_content(self.save_file)
